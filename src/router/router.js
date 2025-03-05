2c5c630ee693aa2474e1861a5ea2c888bb8c3e7a
zeyad0z <zeyadabdelaziz691@gmail.com>
2025-03-05 03:00:12 +0200

    order tracking implementation and some fixing bugs

--- src/router/router.js
+++ src/router/router.js
@@ -1,38 +1,32 @@
+import { createRouter, createWebHistory } from "vue-router";
 
-import { createRouter, createWebHistory } from 'vue-router'
-
-import LoginView from '@/views/auth/LoginView.vue';
-import HomePage from '@/views/HomePage.vue';
-import Register from '@/views/auth/RegisterView.vue'
+import LoginView from "@/views/auth/LoginView.vue";
+import HomePage from "@/views/HomePage.vue";
+import Register from "@/views/auth/RegisterView.vue";
 import CartView from "@/views/CartView.vue";
 
 import CheckoutView from "@/views/CheckoutView.vue";
 
 import DvrDashboard from "@/views/dashboards/DvrDashboard.vue";
 import PlansView from "@/views/PlansView.vue";
-
-
-
+import OrderTrackingView from "@/views/OrderTrackingView.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-
-      path: '/',
-      name: 'Home',
-      component:HomePage
-
+      path: "/",
+      name: "Home",
+      component: HomePage,
     },
     {
-
-      path: '/login',
-      name: 'login',
-      component: LoginView
+      path: "/login",
+      name: "login",
+      component: LoginView,
     },
     {
-      path: '/register',
-      name: 'Register',
-      component: Register
+      path: "/register",
+      name: "Register",
+      component: Register,
     },
     {
       path: "/cart",
@@ -40,12 +34,11 @@ const router = createRouter({
       component: CartView,
     },
     {
-
       path: "/checkout",
       name: "checkout",
       component: CheckoutView,
     },
-
+    {
       path: "/dashboard/delivery",
       name: "dvrDashboard",
       component: DvrDashboard,
@@ -54,8 +47,12 @@ const router = createRouter({
       path: "/meal-plans",
       name: "plans",
       component: PlansView,
-    }
-
+    },
+    {
+      path: "/order-tracking",
+      name: "order-tracking",
+      component: OrderTrackingView,
+    },
   ],
 });
 
