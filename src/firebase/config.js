caa7bd9889a83b0418d941c687c298c722ddea1c
Ziad-Mahmood <ziadmahmoodz.313@gmail.com>
2025-03-06 17:41:53 +0200

    firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCWRj9o1mSmO6kUpmWNXDAQLtH4C5AjTfY",
+  authDomain: "fit-fuel-a552b.firebaseapp.com",
+  projectId: "fit-fuel-a552b",
+  storageBucket: "fit-fuel-a552b.firebasestorage.app",
+  messagingSenderId: "373173305419",
+  appId: "1:373173305419:web:a395f8a4070322b6d08dce"
+};
+
+
+const app = initializeApp(firebaseConfig);
