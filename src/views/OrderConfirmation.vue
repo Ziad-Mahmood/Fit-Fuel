c201a6e78924d1c85ee6b4ab5a6fdc31d6c31651
Sara <sarawaleed8479@gmail.com>
2025-03-09 05:11:12 +0200

    some payment methods

---
+++ src/views/OrderConfirmation.vue
@@ -0,0 +1,12 @@
+<template>
+  <ConfirmPage />
+</template>
+<script>
+import ConfirmPage from "@/components/confirmPage.vue";
+export default {
+  name: "OrderConfirmation",
+  components: {
+    ConfirmPage,
+  },
+};
+</script>
