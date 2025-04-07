04d6a6f4d80de7f9303d2460069d995b8287b083
Sara <sarawaleed8479@gmail.com>
2025-04-07 22:37:05 +0200

    home-ar

--- src/components/HomePage/HomeWork.vue
+++ src/components/HomePage/HomeWork.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="container mx-auto px-4 my-30">
-    <h2 class="text-4xl font-bold text-center my-20">How does it work</h2>
-    <div class="grid grid-cols-2 lg:grid-cols-4 w-[80%] mx-auto gap-8">
+    <h2 class="text-4xl font-bold text-center my-20">{{ $t('homeWork.title') }}</h2>
+    <div 
+      class="grid grid-cols-2 lg:grid-cols-4 w-[80%] mx-auto gap-8"
+      :style="{ 'direction': isRTL ? 'rtl' : 'ltr' }"
+    >
       <div class="text-center" v-for="(work, i) in works" :key="i">
-        <img :src="work.img" :alt="work.title" class="mx-auto mb-4 w-[125px] h-[125px]" />
-        <h3 class="text-xl font-bold mb-2 text-[#434343]">{{ work.title }}</h3>
-        <p class="text-[#434343] text-normal">{{ work.description }}</p>
+        <img :src="work.img" :alt="$t(`homeWork.works.${i}.title`)" class="mx-auto mb-4 w-[125px] h-[125px]" />
+        <h3 class="text-xl font-bold mb-2 text-[#434343]">{{ $t(`homeWork.works.${i}.title`) }}</h3>
+        <p class="text-[#434343] text-normal">{{ $t(`homeWork.works.${i}.description`) }}</p>
       </div>
     </div>
   </div>
 </template>
-
 <script>
 import Icon1 from '@/assets/images/home-Icon1.png'
 import Icon2 from '@/assets/images/home-icon2.png'
@@ -45,7 +47,18 @@ export default {
       ],
     }
   },
+  computed: {
+    isRTL() {
+      return this.$i18n.locale === 'ar';
+    }
+  }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.flex-row-reverse {
+  direction: rtl;
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
